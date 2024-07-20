68dc9583afbab12d0d479af673971518a1736ca2
Tony Santana <tonysantana1492@gmail.com>
2024-07-19 21:55:27 -0400

    chore(): add Tailwind CSS configuration files and update dependencies

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Element from "element-ui";
+import enLang from "element-ui/lib/locale/lang/en";
+import 'element-ui/lib/theme-chalk/index.css';
+import App from "./App.vue";
+import './assets/tailwind.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Element, {
+  size: "medium",
+  locale: enLang,
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
